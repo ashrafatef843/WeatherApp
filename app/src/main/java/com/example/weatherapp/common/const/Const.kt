d4d71bbe5a57cab80e912b2103786073d4d92a04
Ashraf-atef <ashrafatef84@gmail.com>
2023-10-09 17:51:07 +0300

    Implement domain layer and current weather usecase

--- app/src/main/java/com/example/weatherapp/common/const/Const.kt
+++ app/src/main/java/com/example/weatherapp/common/const/Const.kt
@@ -1,2 +1,7 @@
 package com.example.weatherapp.common.const
 
+const val API_KEY = "bbbcbe725f6685f109434c450b919b7a"
+const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
+const val UNIT: String = "metric"
+const val ICON_URL = "http://openweathermap.org/img/wn/"
+const val ICON_EXTENSION = "@4x.png"
